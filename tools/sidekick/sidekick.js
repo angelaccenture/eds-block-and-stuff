b098c8a5ff00900de075e19b37ae6ef8bf48ab30
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-01-19 11:06:10 -0800

    updates

---
+++ tools/sidekick/sidekick.js
@@ -0,0 +1,18 @@
+import toggleScheduler from '../scheduler/scheduler.js';
+import initQuickEdit from '../quick-edit/quick-edit.js';
+
+const getSk = () => document.querySelector('aem-sidekick');
+
+async function ready(sk) {
+  console.log("make sure sidekick is reading this file")
+  sk.classList.add('is-ready');
+  sk.addEventListener('custom:scheduler', toggleScheduler);
+  sk.addEventListener('custom:quick-edit', initQuickEdit);
+}
+
+(async function loadSidekick() {
+  const sk = getSk() || await new Promise((resolve) => {
+    document.addEventListener('sidekick-ready', () => resolve(getSk()));
+  });
+  ready(sk);
+}());
