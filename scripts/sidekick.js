3864939da9de34f5e35123048124b32d48dfe9da
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2026-01-20 08:25:07 -0800

    revertback

--- scripts/sidekick.js
+++ scripts/sidekick.js
@@ -1,17 +1,25 @@
-import initQuickEdit from '../tools/quick-edit/quick-edit.js';
+/* eslint-disable import/no-cycle */
+import { NX_ORIGIN } from './scripts.js';
 
-const getSk = () => document.querySelector('aem-sidekick');
+let expMod;
+const DA_EXP = '/public/plugins/exp/exp.js';
 
-async function ready(sk) {
-  console.log("make sure sidekick is reading this file")
-  sk.classList.add('is-ready');
-  sk.addEventListener('custom:scheduler', toggleScheduler);
-  sk.addEventListener('custom:quick-edit', initQuickEdit);
+async function toggleExp() {
+  const exists = document.querySelector('#aem-sidekick-exp');
+
+  // If it doesn't exist, let module side effects run
+  if (!exists) {
+    expMod = await import(`${NX_ORIGIN}${DA_EXP}`);
+    return;
+  }
+
+  // Else, cache the module here and toggle it.
+  if (!expMod) expMod = await import(`${NX_ORIGIN}${DA_EXP}`);
+  expMod.default();
 }
 
-(async function loadSidekick() {
-  const sk = getSk() || await new Promise((resolve) => {
-    document.addEventListener('sidekick-ready', () => resolve(getSk()));
-  });
-  ready(sk);
+(async function sidekick() {
+  const sk = document.querySelector('aem-sidekick');
+  if (!sk) return;
+  sk.addEventListener('custom:experimentation', toggleExp);
 }());
